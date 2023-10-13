e43797e6d43ef9e5b425c5f32b8baf4065fb4c69
MinhHieuDL <minhhieuhcmute@gmail.com>
2023-10-13 11:26:56 +0700

    Clean code for client app

--- app/ipcSocket/client/src/client.c
+++ app/ipcSocket/client/src/client.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h> 
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h> 
 #include <unistd.h> 
@@ -7,46 +8,41 @@
   
 int main(int argc, char const* argv[]) 
 { 
-    int status, valread, client_fd; 
+    int client_fd; 
     struct sockaddr_in serv_addr; 
     char* hello = "Hello from client"; 
     char buffer[1024] = { 0 }; 
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
+        perror("Socket creation error \n"); 
+        exit(EXIT_FAILURE); 
     } 
   
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(PORT); 
   
-    // Convert IPv4 and IPv6 addresses from text to binary 
-    // form 
-    if (inet_pton(AF_INET, "192.168.1.29", &serv_addr.sin_addr) 
-        <= 0) { 
-        printf( 
-            "\nInvalid address/ Address not supported \n"); 
-        return -1; 
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if (inet_pton(AF_INET, "192.168.1.29", &serv_addr.sin_addr) <= 0) { 
+        printf("Invalid address -- Address not supported \n"); 
+        exit(EXIT_FAILURE); 
     } 
   
-    if ((status 
-         = connect(client_fd, (struct sockaddr*)&serv_addr, 
-                   sizeof(serv_addr))) 
-        < 0) { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
+    if (connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) { 
+        perror("connect Failed"); 
+        exit(EXIT_FAILURE); 
     }
+
     // send data to server 
     if(send(client_fd, hello, strlen(hello), 0) < 0) {
         perror("send failed");
     }
-    printf("Send message to server successed\n");
+    printf("Send message to server successed \n");
     
     // read message from server
     if(read(client_fd, buffer, 1024) < 0) {
         perror("read failed");
     } 
     else {
-        printf("Message from server received: %s\n", buffer);
+        printf("Message from server received: %s \n", buffer);
     }
   
     // closing the connected socket 
