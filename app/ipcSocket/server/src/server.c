d984c8bf45cc46813fbeb60c85a60b6b8462b429
MinhHieuDL <minhhieuhcmute@gmail.com>
2023-11-24 22:23:15 +0700

    handle interrupt signal from user

--- app/ipcSocket/server/src/server.c
+++ app/ipcSocket/server/src/server.c
@@ -4,17 +4,28 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <signal.h>
 #include "remoteHaShaHandler.h"
 #include "verifyLoginHandler.h"
 
 #define PORT 8080
 
+// Global variable to track if termination signal received
+volatile sig_atomic_t terminate = 0;
+
+void sigint_handler(int signum);
 void loginHandling(void* iChannel);
 
 int main(void)
 {
     int iServerDes, iNewChanDes;
     struct sockaddr_in address;
+    
+    // Register signal handler for Ctrl + C (SIGINT)
+    if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        perror("signal failed");
+        exit(EXIT_FAILURE);
+    }
 
     // init server socket 
     address.sin_family = AF_INET;
@@ -28,7 +39,7 @@ int main(void)
     printf("Set up success\n");
     printf("Wait for client connect ...\n");
 
-    while(1) {
+    while(!terminate) {
         // wait for client connect
         if( !WaitForClientConnect(iServerDes, address, &iNewChanDes) ) {
             perror("connection failed! Retry ...");
@@ -70,4 +81,11 @@ void loginHandling(void* iChannel)
     /* close socket and clean up */
 	close(iChannel);
 	pthread_exit(0);
+}
+
+void sigint_handler(int signum) {
+    if (signum == SIGINT) {
+        printf("\nTermination signal received. Shutting down the server...\n");
+        terminate = 1;
+    }
 }
