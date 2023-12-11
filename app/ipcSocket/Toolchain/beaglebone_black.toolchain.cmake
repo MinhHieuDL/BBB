24723aa7921b8796487d1a3a1e6412becfb9c414
MinhHieuDL <minhhieuhcmute@gmail.com>
2023-12-11 11:41:33 +0700

    rename .c to .cpp, update CMake to use CPP compiler for server - Make server buildable

--- app/ipcSocket/Toolchain/beaglebone_black.toolchain.cmake
+++ app/ipcSocket/Toolchain/beaglebone_black.toolchain.cmake
@@ -1,4 +1,4 @@
 # Set the cross-compiler and toolchain file for BeagleBone Black
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_C_COMPILER /home/mhle/Learning/BBB/toolchain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER /home/mhle/Learning/BBB/toolchain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
 set(CMAKE_FIND_ROOT_PATH /home/mhle/Learning/BBB/toolchain/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf)
