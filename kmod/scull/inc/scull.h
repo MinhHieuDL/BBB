4b5d3c03f21cb8c19f859128e8a92cd7686399c1
MinhHieuDL <minhhieuhcmute@gmail.com>
2024-05-17 10:15:12 +0700

    add description for scull device layout

--- kmod/scull/inc/scull.h
+++ kmod/scull/inc/scull.h
@@ -12,6 +12,17 @@
 #define SCULL_NR_DEVS 4    /* scull0 through scull3 */
 #endif
 
+/*
+ * The bare device is a variable-length region of memory.
+ * Use a linked list of indirect blocks.
+ *
+ * "scull_dev->m_pData" points to a scull_qset
+ * "scull_qset->data" points to an array of pointers, each
+ * pointer refers to a memory area of SCULL_QUANTUM bytes.
+ *
+ * The array (quantum-set) is SCULL_QSET long.
+ */
+
 #ifndef SCULL_QUANTUM
 #define SCULL_QUANTUM 4000
 #endif
