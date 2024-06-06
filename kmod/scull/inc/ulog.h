ea21c8ffb3f139c5bf1dce271ac6806401349807
MinhHieuDL <minhhieuhcmute@gmail.com>
2024-06-06 16:56:01 +0700

    add mechanism to dis/enable debug mode

--- kmod/scull/inc/ulog.h
+++ kmod/scull/inc/ulog.h
@@ -3,7 +3,19 @@
 
 #include <linux/kernel.h>
 
-#define ULog(fmt, ...) \
-    printk(KERN_INFO "%s: " fmt, __func__, ##__VA_ARGS__)
+#undef ULOG
+#ifdef SCULL_DEBUG
+    #ifdef __KERNEL__
+        /* This one if debugging is on, and kernel space*/
+        #define ULog(fmt, ...) \
+            printk(KERN_DEBUG "%s: " fmt, __func__, ##__VA_ARGS__)
+    #else
+        /* This one for user space */
+        #define ULog(fmt, ...) \
+            fprintf(stderr, "%s: " fmt, __func__, ##__VA_ARGS__)
+    #endif //__KERNEL__
+#else
+    #define ULog(fmt, args...) /* not debugging: nothing */
+#endif  //SCULL_DEBUG
 
-#endif
+#endif  // _ULOG_H_
